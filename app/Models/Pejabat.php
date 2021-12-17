6f62d0a31c577f6b7a066b32410082a2ba655300
StevenSingging <89833685+StevenSingging@users.noreply.github.com>
2021-12-17 22:48:06 +0700

    FIX

--- app/Models/Pejabat.php
+++ app/Models/Pejabat.php
@@ -10,10 +10,8 @@ class Pejabat extends Model
     use HasFactory;
     protected $guarded = ['id'];
     public function pj(){
-<<<<<<< HEAD
+
         return $this->hasMany(PengajuanSurat::class,'pejabat_id','id');
-=======
-        return $this->hasMany(PengajuanSurat::class,'id');
->>>>>>> 9c161e039e2f7d9175f16a85a899b53f492332a7
+
     }
 }
